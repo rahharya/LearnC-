9ef79d097eee42183c282a2b97434820bd52ab9e
Yohanes Korbafo <rahharya@gmail.com>
2019-05-15 12:52:45 +0200

    Add (Pointer Introduction and Heap Memory Allocation)

--- main.cpp
+++ main.cpp
@@ -3,78 +3,68 @@
 #include <math.h>
 
 using namespace std;
-int _CE30ReversingNumber(int n)
-{
-    int output=0;
-    do
-    {
-        int r=n%10;
-        n/=10;
-        output=output*10+r;
-    } while (n>0);    
-    //cout<<output<<endl;    
-    return output;
-}
-
-void _CE31IsPalindrome(int n)
-{
-    int output = _CE30ReversingNumber(n);
-    if(n==output) cout<<n<<" palindrome"<<endl;
-    else cout<<n<< " not a palindrome"<<endl;
-}
-
-void _81GreatestCommonDivider(int a, int b)
-{
-    while(a!=b)
-    {
-        if(a>b) a-=b;
-        else b-=a;
-    }
-    cout<<"Greatest Common Divider is "<<a<<endl;
-}
-
-void _85C11NewFeatureWithAuto()
-{
-    float fA[]={3,6.56f,7.345f,6.123f}; // sometimes we don't need to decleare the size of the Array
-    cout<<"New Feature from C11 : auto datatype for Array or Vector"<<endl; cout<<"A[]: ";
-    for(auto x:fA) cout<<x<<" "; cout<<endl;//it works like for each, with auto datatyp that means that it will detect the datatyp from A by itself
-    // it can also written like this when we already now what datatype that the Array/Vector
-    //for(char i:A) cout<<i<<" ";
-
-    //Summe from all Element of the Array
-    float fsum=0.0f;
-    for(auto x:fA) fsum=fsum+x;
-    cout<<"Sum: "<<fsum<<endl;
-        //or
-        //float sum=0.0f; for (int i=0; i<4; i++) sum=sum+fA[i]; cout<<"Sum: "<<sum<<endl;
-    
-    //Finding the max Element from an Array
-    float fmax=fA[0];
-    for(auto x:fA)
-    {
-        if (x>fmax) fmax=x;
-    }
-    cout<<"Max: "<<fmax<<endl;
-}
 
 int main(void)
 {
-    //CE30 and CE31
-    /*
-        int nNumber;
-        cout<<"Enter n: ";
-        cin>>nNumber;
-        _CE31IsPalindrome(nNumber);
+    //103. Pointer Introduction
+    cout<<"---==Pointer Introduction==---"<<endl<<endl;
+
+    /* Pointer is to used to:
+        1. Accesing Heap Memory
+        2. Accesing File, Keyboard, Printer or another Hardware that connected
+    */ 
+
+    int x=10; 
+    int *px=&x; //Declaration and Initialisation of Pointer
+
+    /* It can also be written like this
+        int x=10;
+        int *px;    //Initialisation of Pointer
+        px=&x;      //Declaration the value of px with Adresse of x
     */
-    //81
-    int na,nb;
-    _85C11NewFeatureWithAuto();
-    start:
-    
-    // cout<<"Enter a and b: "; cin>>na>>nb; _81GreatestCommonDivider(na,nb); cout<<endl;
-    
-    goto start;
+
+    cout<<"Value of integer x        : "<<x<<endl;
+    cout<<"Adresse of Integer x      : "<<&x<<endl;
+    cout<<"Value of Pointer px       : "<<px<<endl;
+    cout<<"Adresse of Pointer px     : "<<&px<<endl;
+    cout<<"The value that stored in x: "<<*px<<endl;  //Dereferencing Pointer
+    cout<<endl;
+
+    cout<<"---==Heap Memory Allocation==---"<<endl<<endl;
+
+    // Variable that located in the Stack will be automaticlly deleted when get out of the scope
+    // For Variable that located in Heap will be still there until the Program end or until the Memory deleted
+    // Therefore it's important to delocated memory that not used (delete)
+    // When the variable that in Heap located not pointed by any pointer that mean we can't access or delete the variable
+    // and that is called 'Memory Leak' 
+
+    int nHeapArraySize;
+    int A[5]={1,2,3,4,5};   // Stack Array (the size of the Stack Array is cannot be modified)
+    cout<<"Enter size of the Heap Array: "; cin>>nHeapArraySize;
+    int *pA = new int[nHeapArraySize];      // Pointer located in Stack and that pointer will be 
+                                            // pointing Array that located in Heap Memory
+
+    //Modify the size of Heap Array                                        
+    cout<<"Enter new Size of the same Heap Array: "; cin>>nHeapArraySize;
+    delete [] pA;                           // delete heap memory that pA pointed
+    pA = new int[nHeapArraySize];           // This will be pointing the same HeapArray because it's using the same Pointer 
+                                            // That means with HeapArray we can modify it's size 
+    pA[4]=5;                // [0][0][5][0][0] -> HEAP
+    /* It can also be written like this
+        int *pA;
+        pA = new int[5];
+    */
+    delete [] pA;           // delete heap memory that pA pointed
+    pA=NULL;                // delete the value of the pA that already deleted
+    /*Note:
+        it's important to delete the memory first before setting the pointer value to null
+        when the pointer deleted first before the heap memory deleted that it will only delete
+        the adresse of the Heap Variable but wont delete the Variable from the heap (called 'MEMORY LEAK')
+    */
+
+    cout<<"---==Pointer Arithmetic==---"<<endl<<endl;
     system("PAUSE");
     return 0;
+
 }
 
