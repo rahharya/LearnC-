338d38dc748a633ed6beb2959df7588c8d8069f1
Yohanes Korbafo <rahharya@gmail.com>
2019-05-17 10:56:07 +0200

    Create Class.h and Datahiding

--- main.cpp
+++ main.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <string>
 #include <math.h>
-
+#include "class.h"
 using namespace std;
 
 
-void _85C11NewFeatureWithAuto()
-{
-    float fA[]={3,6.56f,7.345f,6.123f}; // sometimes we don't need to decleare the size of the Array
-    cout<<"New Feature from C11 : auto datatype for Array or Vector"<<endl; cout<<"A[]: ";
-    for(auto x:fA) cout<<x<<" "; cout<<endl;//it works like for each, with auto datatyp that means that it will detect the datatyp from A by itself
-    // it can also written like this when we already now what datatype that the Array/Vector
-    //for(char i:A) cout<<i<<" ";
 
-    //Summe from all Element of the Array
-    float fsum=0.0f;
-    for(auto x:fA) fsum=fsum+x;
-    cout<<"Sum: "<<fsum<<endl;
-        //or
-        //float sum=0.0f; for (int i=0; i<4; i++) sum=sum+fA[i]; cout<<"Sum: "<<sum<<endl;
-    
-    //Finding the max Element from an Array
-    float fmax=fA[0];
-    for(auto x:fA)
-    {
-        if (x>fmax) fmax=x;
-    }
-    cout<<"Max: "<<fmax<<endl;
-}
 
 int main(void)
 {
-    cout<<"Hello World"<<endl;
+    //141. Demo - Class in C++;
+        Rectangle r1; // Object Rectangle created in Stack in this sense are the Datamembers saved
+        r1.length=10; r1.width=5;
+        cout<<"Area of r1      :"<<r1.area()<<endl;
+        cout<<"perimeter of r1 :"<<r1.perimeter()<<endl;
+        cout<<endl;
+    //142. Pointer to an Object in Heap
+        //Pointer to Object in Stack
+            Rectangle r2;
+            Rectangle *pR2;
+            pR2=&r2; // Pointer pR2 have the Address of Rectangle r2
+            //How to access public Funktion or datamember that pointed by pointer
+                pR2->length=20; pR2->width=4;
+                cout<<"Area of r2      :"<<pR2->area()<<endl;
+                cout<<"perimeter of r2 :"<<pR2->perimeter()<<endl;
+                cout<<endl;
+        Rectangle *pHeapRect = new Rectangle; // Pointer pointing to Rectangle that created in Heap;
+        pHeapRect->length=10;
+        pHeapRect->width=22;
+        cout<<"Area of pHeapRect      :"<<pHeapRect->area()<<endl;
+        cout<<"perimeter of pHeapRect :"<<pHeapRect->perimeter()<<endl;
+        delete pHeapRect;
+        pHeapRect=nullptr;
     system("PAUSE");
     return 0;
 }
