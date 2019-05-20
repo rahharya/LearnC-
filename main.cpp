21b48314116bd6cae699021f1e233814e0006a7d
Yohanes Korbafo <rahharya@gmail.com>
2019-05-20 13:45:12 +0200

    Clean Code

--- main.cpp
+++ main.cpp
@@ -7,50 +7,8 @@
 
 using namespace std;
 
-//SECTION 11 INTRODUCTION TO OOPS
-    void IntroductiontoOOPS()
-    {
-    //141. Demo - Class in C++;
-        Rectangle r1; // Object Rectangle created in Stack in this sense are the Datamembers saved
-        r1.length=10; r1.width=5;
-        cout<<"Area of r1      :"<<r1.area()<<endl;
-        cout<<"perimeter of r1 :"<<r1.perimeter()<<endl;
-        cout<<endl;
-    //142. Pointer to an Object in Heap
-        //Pointer to Object in Stack
-            Rectangle r2;
-            Rectangle *pR2;
-            pR2=&r2; // Pointer pR2 have the Address of Rectangle r2
-            //How to access public Funktion or datamember that pointed by pointer
-                pR2->length=20; pR2->width=4;
-                cout<<"Area of r2      :"<<pR2->area()<<endl;
-                cout<<"perimeter of r2 :"<<pR2->perimeter()<<endl;
-                cout<<endl;
-        Rectangle *pHeapRect1 = new Rectangle; // Pointer pointing to Rectangle that created in Heap;
-        pHeapRect1->length=10;
-        pHeapRect1->width=22;
-        cout<<"Area of pHeapRect      :"<<pHeapRect1->area()<<endl;
-        cout<<"perimeter of pHeapRect :"<<pHeapRect1->perimeter()<<endl<<endl;
-        delete pHeapRect1;
-        pHeapRect1=nullptr;
-    //146. Constructor, Accesor and Mutator 
-        HideRectangle r3;
-        HideRectangle *pHeapRect2= new HideRectangle(r3);
-        pHeapRect2->setLength(10);
-        pHeapRect2->setWidth(20);
-        cout<<"Area of r3      :"<<r3.area()<<endl;
-        cout<<"perimeter of r3 :"<<r3.perimeter()<<endl<<endl;
-        cout<<"Area of pHeapRect2       :"<<pHeapRect2->area()<<endl;
-        cout<<"perimeter of pHeapRect2  :"<<pHeapRect2->perimeter()<<endl<<endl;
-        pHeapRect2->inLineFunction();
-        pHeapRect2->nonInLineFunction();
-    }
-
-
-int main(void)
+void Section12_Operator_Overloading()
 {
-    //IntroductiontoOOPS(); // UNCOMMENT TO USE SECTION 11
-    
     Complex c1(1,3), c2(4,2), c3a, c3b;
     cout<<"c1 = "; c1.Display();
     cout<<"c2 = "; c2.Display();
@@ -76,6 +34,10 @@ int main(void)
         cout<<"         r3 = r1+r2b : "<<r3<<endl;
         r4=r2b-r1;
         cout<<"         r4 = r2b+r1 : "<<r4<<endl;
+}
+int main(void)
+{
+    
     system("PAUSE");
     return 0;
 }
